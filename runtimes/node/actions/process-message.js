db46919fe50a8acbf431f55045ffc8be4c903f00
Belinda Vennam <beemarie@us.ibm.com>
2017-10-09 16:49:49 -0500

    initial commit

---
+++ runtimes/node/actions/process-message.js
@@ -0,0 +1,24 @@
+function main(params) {
+
+  console.log(params);
+
+  return new Promise(function(resolve, reject) {
+    if (!params.messages || !params.messages[0] || !params.messages[0].value) {
+      reject("Invalid arguments. Must include 'messages' JSON array with 'value' field");
+    }
+    var msgs = params.messages;
+    var cats = [];
+    for (var i = 0; i < msgs.length; i++) {
+      var msg = msgs[i];
+      for (var j = 0; j < msg.value.cats.length; j++) {
+        var cat = msg.value.cats[j];
+        console.log('A ' + cat.color + ' cat named ' + cat.name + ' was received.');
+        cats.push(cat);
+      }
+    }
+    resolve({
+      "cats": cats
+    });
+  });
+
+}
