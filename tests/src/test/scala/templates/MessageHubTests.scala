3fe5af6dbfa33a0744956a4ef3b27b08e8bfa227
Belinda Vennam <beemarie@us.ibm.com>
2018-02-20 11:25:54 -0800

    updating runtimes folders

--- tests/src/test/scala/templates/MessageHubTests.scala
+++ tests/src/test/scala/templates/MessageHubTests.scala
@@ -35,10 +35,10 @@ class MessageHubTests extends TestHelpers
 
     //set parameters for deploy tests
     val nodejs6folder = "../runtimes/nodejs-6/actions";
-    val nodejs8folder = "../runtimes/nodejs-8/actions";
-    val phpfolder = "../runtimes/php-7.1/actions";
-    val pythonfolder = "../runtimes/python-3.6.4/actions";
-    val swiftfolder = "../runtimes/swift-3.1.1/actions";
+    val nodejs8folder = "../runtimes/nodejs/actions";
+    val phpfolder = "../runtimes/php/actions";
+    val pythonfolder = "../runtimes/python/actions";
+    val swiftfolder = "../runtimes/swift/actions";
 
     behavior of "MessageHub Template"
     val catsArray = Map("cats" -> JsArray(JsObject(
